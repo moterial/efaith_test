0215e3842e870601559d35600e9ed7247a1a22a3
Michael Yeung <74708587+moterial@users.noreply.github.com>
2023-05-24 23:53:31 +0800

    update

---
+++ database/migrations/2023_05_24_141701_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longtext('content');
+            $table->string('image');
+            $table->string('author');
+            $table->boolean('is_published')->default(false);
+            $table->string('youtube_link')->nullable();
+            $table->foreignId('gallery_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
